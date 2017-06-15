c966d8ce8a32c1e9d90de257d10edda3af9c4a77
Anant Ambhaikar <aambhaik@tibco.com>
2017-06-15 12:27:29 -0700

    add more logs

--- util/jsonpath.go
+++ util/jsonpath.go
@@ -3,6 +3,8 @@ package util
 import (
 	"github.com/NodePrime/jsonpath"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/pkg/errors"
+	"fmt"
 )
 
 var flogoLogger = logger.GetLogger("trigger-tibco-kafkasubv2")
@@ -16,6 +18,7 @@ func JsonPathEval(jsonData string, expression string) (*string, error) {
 
 	eval, err := jsonpath.EvalPathsInBytes([]byte(jsonData), paths)
 	if err != nil {
+		flogoLogger.Infof("uable to eval expression, error is [%v]", err)
 		return nil, err
 	}
 
@@ -26,8 +29,9 @@ func JsonPathEval(jsonData string, expression string) (*string, error) {
 			flogoLogger.Infof("expression parsed is [%v], value is [%v]", expression, value)
 			return &value, nil // true -> show keys in pretty string
 		} else {
+			flogoLogger.Infof("evaluation failed..")
 			break
 		}
 	}
-	return nil, nil
+	return nil, errors.New(fmt.Sprintf("Error with expression[%v] on content [%v]", expression, jsonData))
 }
